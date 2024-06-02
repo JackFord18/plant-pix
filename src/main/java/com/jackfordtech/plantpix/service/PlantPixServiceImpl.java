cba4de143ca70198cb418a1359dd499a2d2e9398
JackFord18 <jdford2@wisc.edu>
2024-06-02 16:18:35 -0500

    Added functionality for downloading the last-updated image

--- src/main/java/com/jackfordtech/plantpix/service/PlantPixServiceImpl.java
+++ src/main/java/com/jackfordtech/plantpix/service/PlantPixServiceImpl.java
@@ -4,18 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.InvalidMediaTypeException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.*;
 
 @Service
 public class PlantPixServiceImpl implements PlantPixService {
     @Autowired
     S3Client s3Client;
+
+    public byte[] latestImage() {
+        GetObjectRequest objectRequest = constructGetObjectRequest("latest/latestImage.jpg");
+        ResponseBytes<GetObjectResponse> objectBytes = s3Client.getObjectAsBytes(objectRequest);
+
+        return objectBytes.asByteArray();
+    }
 
     public void uploadImage(MultipartFile file) throws IOException {
         String defaultImageType = "jpg";
@@ -29,11 +40,13 @@ public class PlantPixServiceImpl implements PlantPixService {
         byte[] contents = file.getBytes();
         String awsKey = getAwsKey(defaultImageType);
 
-        Map<String, String> metadata = new HashMap<>();
-        s3Client.putObject(PutObjectRequest.builder().metadata(metadata).bucket("plantpix").key(awsKey).build(), RequestBody.fromBytes(contents));
+        s3Client.putObject(PutObjectRequest.builder().bucket("plantpix").key(awsKey).build(), RequestBody.fromBytes(contents));
+        if(shouldUpdateLatestImage(s3Client)){
+            s3Client.putObject(PutObjectRequest.builder().bucket("plantpix").key("latest/latestImage.jpg").build(), RequestBody.fromBytes(contents));
+        }
     }
 
-    private static String getAwsKey(String defaultImageType) {
+    private String getAwsKey(String defaultImageType) {
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
         String formattedDate = sdf.format(date);
@@ -46,5 +59,18 @@ public class PlantPixServiceImpl implements PlantPixService {
         String second = formattedDate.substring(12,14);
         String millisecond = formattedDate.substring(14,17);
         return String.format("%s/%s/%s/%s/%s/%s%s.%s", year, month, day, hour, minute, second, millisecond, defaultImageType);
+    }
+
+    private GetObjectRequest constructGetObjectRequest(String key){
+        return GetObjectRequest.builder().bucket("plantpix").key(key).build();
+    }
+
+    private boolean shouldUpdateLatestImage(S3Client s3Client) {
+        Instant currentTime = Instant.now();
+        GetObjectRequest getObjectRequest = constructGetObjectRequest("latest/latestImage.jpg");
+        GetObjectResponse getObjectResponse = s3Client.getObject(getObjectRequest).response();
+        Instant latestImageLastModified = getObjectResponse.lastModified();
+
+        return currentTime.isAfter(latestImageLastModified);
     }
 }
